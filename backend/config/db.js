1964150fd4a38caf2822f8e3688ed35739e3bb8d
rajnishpatell <rajnishpatel.1642003@gmail.com>
2026-06-01 22:05:57 +0530

    logging instead of console statements in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    logger.info(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("Database connection failed:", error.message);
     process.exit(1);
